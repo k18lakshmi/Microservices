9c97c4509d0637295c12acea7cd05309c4da758a
Lakshmi K <92250908+k18lakshmi@users.noreply.github.com>
2021-10-22 20:06:44 +0530

    Add files via upload

---
+++ moviecatalogservice/src/test/java/com/tcs/moviecatalogservice/MoviecatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ movieinfoservice/src/test/java/com/tcs/movieinfoservice/MovieinfoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieinfoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
